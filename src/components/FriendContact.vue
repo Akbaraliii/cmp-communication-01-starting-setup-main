4c6f9d8b83f656597c17fc3bafe4abb38f469d2f
Azamjon <akbaraliev0707@gmail.com>
2022-09-13 14:46:05 +0900

    prop behavior

--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -1,19 +1,17 @@
 <template>
   <li>
-    <h2></h2>
-    <h2></h2>
-    <h2>{{ friend.name }}</h2>
+    <h2>{{ name }}</h2>
     <button @click="toggleDetails">
       {{ detailsAreVisible ? 'Hide' : 'Show' }} Details
     </button>
     <ul v-if="detailsAreVisible">
       <li>
         <strong>Phone:</strong>
-        {{ friend.phone }}
+        {{ phoneNumber }}
       </li>
       <li>
         <strong>Email:</strong>
-        {{ friend.email }}
+        {{ emailAddress }}
       </li>
     </ul>
   </li>
@@ -21,6 +19,7 @@
 
 <script>
 export default {
+  props: ['name', 'phoneNumber', 'emailAddress'],
   data() {
     return {
       detailsAreVisible: false,
